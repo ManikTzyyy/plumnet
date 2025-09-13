6812fd25e2d91c6e7126ff5b65299904ad6f3456
ManikTzyyy <manikyogantara1@gmail.com>
2025-09-13 15:15:42 +0800

    register model: Transaction

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Gateway, Server, IPPool, Paket, Client, Redaman
+from .models import Gateway, Server, IPPool, Paket, Client, Redaman, Transaction
 
 admin.site.register(Server)
 admin.site.register(IPPool)
@@ -7,3 +7,4 @@ admin.site.register(Paket)
 admin.site.register(Client)
 admin.site.register(Redaman)
 admin.site.register(Gateway)
+admin.site.register(Transaction)
